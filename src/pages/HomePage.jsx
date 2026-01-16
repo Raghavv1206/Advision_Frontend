80cc5222a49a7a0c7207940d499111434b63d238
Raghavv1206 <raghavm205@gmail.com>
2026-01-16 07:57:12 +0530

    time pop up

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,62 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 
 export default function HomePage() {
   const navigate = useNavigate();
+
+  const [showPopup, setShowPopup] = useState(false);
+  useEffect(() => {
+    const hasSeenPopup = sessionStorage.getItem("advision_first_visit");
+
+    if (!hasSeenPopup) {
+      setShowPopup(true);
+      sessionStorage.setItem("advision_first_visit", "true");
+    }
+  }, []);
+
+
 
   return (
     <div className="relative h-screen w-screen overflow-hidden text-white flex flex-col">
 
       {/* NAVBAR */}
+
+      {showPopup && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/70 backdrop-blur-sm">
+          <div className="max-w-md w-full mx-4 rounded-2xl border border-white/20 
+      bg-gradient-to-br from-[#2a2630] to-[#3a3440] p-6 shadow-2xl text-center">
+
+            <h2 className="text-xl font-semibold mb-3 text-[#D6BCEB]">
+              Please Note
+            </h2>
+
+            <p className="text-sm text-white/80 leading-relaxed mb-5">
+              Our backend is hosted on{" "}
+              <span className="text-[#BDA8C8] font-medium">Render’s free plan</span>.
+              When inactive, the server goes to sleep, so the{" "}
+              <span className="font-semibold">
+                first login request may take up to ~50 seconds
+              </span>{" "}
+              while the server starts.
+              <br /><br />
+              This happens only once — after that, the app runs smoothly.
+            </p>
+
+            <button
+              onClick={() => setShowPopup(false)}
+              className="px-6 py-2 rounded-full text-sm font-semibold 
+          bg-gradient-to-r from-[#a88fd8] to-[#C8A9D0]
+          hover:scale-105 transition-all duration-300"
+            >
+              Got it 👍
+            </button>
+          </div>
+        </div>
+      )}
+
+
       <header
         className="absolute top-0 left-0 right-0 z-20 flex justify-between items-center px-9 py-3
           bg-transparent hover:bg-black/50 backdrop-blur-[3px] border-b border-white/10 
